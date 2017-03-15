45fc8eaa14144aa42fbf00afa400719d1e230489
Rick Mu <rickmusa@126.com>
2017-03-15 14:37:08 +0800

    input.txt

--- Simulation.java
+++ Simulation.java
@@ -5,10 +5,11 @@ public class Simulation {
 
     public static void main(String[] args){
 
-        String[][] bd=Utility.readInputFile("C:\\Users\\Rick\\IdeaProjects\\RainDrop-Droptops\\src\\inputs");
+        String[][] bd=Utility.readInputFile("C:\\Users\\Rick\\IdeaProjects\\RainDrop-Droptops\\src\\inputs.txt");
 
         Board board = new Board(bd);
         System.out.println(board.BoardSize());
         System.out.println(board.getCell(0,0));
+        board.printBoard();
     }
 }
