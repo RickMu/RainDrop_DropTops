a5c853bc75f0a86caab9c0dc4e7e4ed34e5e285b
ibrahimahmed4 <ibrahim.m.ahmed1@gmail.com>
2017-03-17 13:11:34 +1100

    Add files via upload

--- Board.java
+++ Board.java
@@ -4,22 +4,35 @@ public class Board {
 	private int dimensions;
 	private String[][] board;
 
-	public Board( String[][] newboard) {
+	public Board(String[][] newboard) {
 		this.dimensions = newboard.length;
-		this.board = newboard;
+		int i,j;
+		board = new String[dimensions][dimensions];
+		for (i=0;i<dimensions;i++) {
+			for (j=0;j<dimensions;j++) {
+				board[i][j] = newboard[i][j];
+			}
+		}
+		//this.board = newboard;
 	}
-
-	public String[][] getBoard(){
-		return this.board;
-	}
+	/** Returns the size of the board. */
 	public int BoardSize() {
 		return dimensions;
 	}
-	
+	/** Gets the value of the cell in (i,j). */
 	public String getCell(int i, int j) {
-		return board[i][j];
+		String thiscell = board[i][j];
+		return thiscell;
 	}
-
+	/** Changes the value of the cell (i,j) for this board. */
+	public void changeCell(int i, int j, String value) {
+		this.board[i][j] = value;
+	}
+	/** Returns the 2d array of this board. */
+	public String[][] getBoard() {
+		return this.board;
+	}
+	/** Prints the board. */
 	public void printBoard(){
 		for(String[] row: board) {
 			for (String cell : row)
@@ -27,10 +40,29 @@ public class Board {
 			System.out.println();
 		}
 	}
-	public boolean cellMoveable(int i, int j) {
+
+	/** Rotates board clockwise. */
+	public static Board rotateBoard(Board board) {
+		Board rotatedBoard = new Board(board.getBoard());
+		int ri, rj;
+		for(ri=0; ri < rotatedBoard.dimensions;ri++) {
+			for (rj=0; rj < rotatedBoard.dimensions;rj++) {
+				//System.out.println("rotated=(" + rj + "," + (dimensions-1-ri) + ") old=(" + ri + "," + rj + ")");
+				rotatedBoard.changeCell(rj, rotatedBoard.dimensions-1-ri, board.getCell(ri, rj));
+			}
+		}
+		return rotatedBoard;
+	}
+	
+	public boolean pieceMoveable(int i, int j) {
+		// Check right.
+		if (j+1 <= dimensions && board[i][j+1] == "+") {return true;} 
+		// Check up.
+		if (i-1 < 0 && board[i][j+1] == "+" ) {return true;} 
+		// Check down.
+		if (i+1 <=dimensions && board[i+1][j] == "+" ) {return true;}
 		return false;
 	}
-
-
-
+	
+	
 }
